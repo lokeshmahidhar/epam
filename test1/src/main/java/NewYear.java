84f73a096bb43760878350c4feea298114e602c3
lokeshmahidhar <53259464+lokeshmahidhar@users.noreply.github.com>
2020-02-08 17:06:33 +0530

    Update NewYear.java

--- test1/src/main/java/NewYear.java
+++ test1/src/main/java/NewYear.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
  class Gift {
-// initially gift box contains the no. of chocolate, candy and sweet as 0 
-	int chocolates = 0, sweets = 0;	// numbers
-	int choc_w = 0, sweet_w = 0;		// weights
+
+	int chocolates = 0, sweets = 0;	
+	int choc_w = 0, sweet_w = 0;	
 }
-
-
-
 class Chocolate extends Gift {
-	// Initialize three dynamic list for storing the three weight objects separately.  
 	public int[] choc;
 	public int[] sweet;
-	
-	
-	// Sort function to sort in ascending order
+
 		public void chocolate_sort(int chocolates) {
 			int temp = 0;
 			for(int i = 0; i < chocolates-1; i++) {
@@ -43,15 +37,13 @@ class Chocolate extends Gift {
 	public class NewYear {
 
 		@SuppressWarnings("unused")
-		private static int opt;
-
-		// Program executes from this method
+		private static int option;
 		@SuppressWarnings("resource")
 		public static void main(String[] args) {
-			// Object of class Chocolate and Scanner
+			
 			Chocolate ch = new Chocolate();
 			Scanner scan = new Scanner(System.in);
-			opt = 0;
+			option = 0;
 			do
 			{
 			System.out.print("\n\t Enter 1. Chocolate, 2.Sweets :- ");
@@ -67,7 +59,6 @@ class Chocolate extends Gift {
 							ch.choc[i] = ch.choc_w;
 						}
 						break;
-			
 				case 2: System.out.print("\n\t\t sweets you want to add : ");
 						ch.sweets = scan.nextInt();
 						ch.sweet = new int[ch.sweets];
@@ -79,24 +70,17 @@ class Chocolate extends Gift {
 						break;
 			}
 			System.out.print("\n\t\t continue(1) or not(0) : ");
-			opt = scan.nextInt();
+			option = scan.nextInt();
 			
-		} while (opt == 1);
+		} while (option == 1);
 		
-			
-			//  calling the sort() to sort all the dynamic lists  
-			//ch.sort();
-			
 			ch.chocolate_sort(ch.chocolates);
 			
 			ch.sweet_sort(ch.sweets);
-			
-			// printing those lists from end to beginning
 			System.out.println("chocolates are : ");
 			for (int i = 0; i < ch.chocolates; i++) {
 				System.out.println(ch.choc[i]);
 			}
-			
 			System.out.println("sweets are : ");
 			for (int i = 0; i < ch.sweets; i++) {
 				System.out.println(ch.sweet[i]);
